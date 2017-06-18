f88982fcadf6861d46dcd9dcef70c3565c9601b8
kbm <kbm0417@nate.com>
2017-06-18 23:07:51 +0900

    2017.06.18

---
+++ src/main/java/com/like/code/infra/jparepository/springdata/JpaCommonCode.java
@@ -0,0 +1,11 @@
+package com.like.code.infra.jparepository.springdata;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.like.code.domain.model.CommonCode;
+
+@Repository
+public interface JpaCommonCode extends JpaRepository<CommonCode, Long> {
+
+}
