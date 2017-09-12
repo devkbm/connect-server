0281acb389d05ec63d62b782c4fff981fae18453
devkbm <kbm0417@nate.com>
2017-09-12 18:43:07 +0900

    2017.09.12

--- src/main/java/com/like/user/web/UserController.java
+++ src/main/java/com/like/user/web/UserController.java
@@ -1,13 +1,19 @@
 package com.like.user.web;
 
-import javax.annotation.Resource;
+import java.util.List;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.like.common.web.util.WebControllerUtil;
+import com.like.user.domain.model.User;
 import com.like.user.service.UserService;
 
 @RestController
@@ -18,8 +24,29 @@ public class UserController {
 	
 	@RequestMapping(value={"/user/login"}, method=RequestMethod.GET) 
 	public ResponseEntity<?> login(@RequestParam(value="id", required=true) String id,
-									@RequestParam(value="pwd", required=true) String password) {
+									@RequestParam(value="pwd", required=true) String password,
+									HttpSession session) {
 		
-		return null;
+		ResponseEntity<?> result = null;			
+		User user = userService.getUser(id);
+						
+		boolean isValid = userService.validPassword(user, password);
+		
+		if (isValid) {
+			setSessionInfo(session, user);
+		}		
+		
+		result = WebControllerUtil.getResponse(null,
+				 1, 
+				 true,
+				 "로그인 처리되었습니다.",
+				 HttpStatus.OK); 					
+		
+		return result;
+	}
+	
+	private void setSessionInfo(HttpSession session, User user) {
+		session.setAttribute("userId", 		user.getUsername());
+		session.setAttribute("userName", 	user.getName());
 	}
 }
